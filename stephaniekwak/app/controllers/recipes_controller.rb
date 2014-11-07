593fe56062f7f584fa66454bd1b5af8a0f4287ce
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-06 21:48:36 -0800

    unrestful delete, form partials, many things

--- stephaniekwak/app/controllers/recipes_controller.rb
+++ stephaniekwak/app/controllers/recipes_controller.rb
@@ -5,20 +5,44 @@ class RecipesController < ApplicationController
   end
 
   def new
-  
     @ingredients = Ingredient.all
+    @recipe = Recipe.new
   end
 
   def create
     @recipe_form = RecipeForm.new(params[:recipe_form])
     if @recipe_form.submit
       redirect_to recipe_path(@recipe_form.recipe)
+    else
+      :new
     end
   end
 
   def show
     find_recipe
   end
+
+  def edit
+    find_recipe
+    @ingredients = Ingredient.all
+  end
+
+  def update
+    @recipe_form = RecipeForm.update(params[:recipe_form])
+    if @recipe_form.submit
+      redirect_to recipe_path(@recipe_form.recipe)
+    else
+      :edit
+    end
+  end
+
+  def destroy
+    find_recipe
+    if @recipe.delete
+      redirect_to recipes_path
+    end
+  end
+
 
   private
 
