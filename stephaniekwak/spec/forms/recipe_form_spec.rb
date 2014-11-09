08834b62da83efd3b4624b4d4092c54cd70264c7
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-08 18:46:08 -0800

    add RecipeForm test

--- stephaniekwak/spec/forms/recipe_form_spec.rb
+++ stephaniekwak/spec/forms/recipe_form_spec.rb
@@ -2,33 +2,42 @@ require "rails_helper"
 
 describe RecipeForm do
   before(:each) do
-    @attributes = { title: "Hot Dog", description: "crazy bratwurst", yield: "1 hotdog", time: "1 minute", directions: "buy hotdog", ingredients: [11, 22] }
+    attributes = { title: "Hot Dog", description: "crazy bratwurst", yield: "1 hotdog", time: "1 minute", directions: "buy hotdog", ingredients: [11, 22], measurement: "22 cups" }
     @recipe_form = RecipeForm.new(attributes)
+    @recipe_form.submit
   end
 
-  
+  it "adds a value to a recipe attribute" do
+    expect(@recipe_form.recipe.title).to eq "Hot Dog"
+  end
+
 end
 
-# def initialize(attributes)
-#   @attributes = attributes
-# end
+# class RecipeForm
 #
-# def recipe
-#   @recipe
-# end
+#   def initialize(attributes)
+#     @attributes = attributes
+#   end
 #
-# def submit
-#   # create an instance of Recipe class
-#   # create recipe ingredients for each id in the ingredients key
-#   @recipe = Recipe.create(
-#     title: @attributes[:title],
-#     description: @attributes[:description],
-#     yield: @attributes[:yield],
-#     time: @attributes[:time],
-#     directions: @attributes[:directions],
-#   )
+#   def recipe
+#     @recipe
+#   end
 #
-#   @attributes[:ingredients].each do |id|
-#     RecipeIngredient.create(ingredient_id: id, recipe_id: @recipe.id, measurement: measurement)
+#   def submit
+#     # create an instance of Recipe class
+#     # create recipe ingredients for each id in the ingredients key
+#     @recipe = Recipe.create(
+#       title: @attributes[:title],
+#       description: @attributes[:description],
+#       yield: @attributes[:yield],
+#       time: @attributes[:time],
+#       directions: @attributes[:directions],
+#     )
+#
+#     @attributes[:ingredients].each do |id|
+#       RecipeIngredient.create(ingredient_id: id, recipe_id: @recipe.id, measurement: @attributes[:measurement])
+#     end
+#
+#
 #   end
 # end
